161fada64aff4948c805c9f759911c3bd73cb7f8
grace <graceogwe@gmail.com>
2024-05-19 00:17:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import "./App.css"
-
 function App(){
-    return(
-        <div className='app'>
-            <h2>This is just a test Website for Hosting on Render</h2>
+    return (
+        <div className="app">
+            <h1>This a Demo Page for Deployment on Render free hosting service</h1>
+
+            <button id='btn1'>Explore now</button>
         </div>
     )
 }
+
 export default App
